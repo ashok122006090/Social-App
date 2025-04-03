f587c56791b2677af1ae77e23799bfc2ead3d6ed
ashok122006090 <yellaashok8@gmail.com>
2025-04-03 11:04:11 +0530

    appd commit

--- src/main/java/Social_App/com/MessageController.java
+++ src/main/java/Social_App/com/MessageController.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 @RestController
 @RequestMapping("/messages")
+
+
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS})
+
 //@CrossOrigin(origins = "*")  // Allow all origins (Frontend can access)
-@CrossOrigin(origins = "https://social-app-frontend-sooty.vercel.app")
+//@CrossOrigin(origins = "https://social-app-frontend-sooty.vercel.app")
 
 public class MessageController {
 
